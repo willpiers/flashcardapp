b17f1900ff3c5f7ed57a7cec3a64c223cc889eb4
Will Piers <wpiers0405@gmail.com>
2013-02-10 21:20:30 -0600

    added a sweet validation that stops users from adding a person to the same group twice

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,8 @@
 class Membership < ActiveRecord::Base
   attr_accessible :group_id, :person_id
 
+  validates_uniqueness_of :group_id, :scope => [:person_id], message: "- That person is already in that group"
+
   belongs_to :person
   belongs_to :group
 
