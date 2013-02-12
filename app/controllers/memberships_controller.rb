e35013040bbd3fab965c336cf6c6eecc039747ed
Will Piers <wpiers0405@gmail.com>
2013-02-12 00:38:26 -0600

    still working on alerts and notices

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -8,7 +8,7 @@ class MembershipsController < ApplicationController
         format.html { redirect_to group_url(@membership.group_id), notice: 'Membership was successfully created.' }
         format.json { render json: memberships_url, status: :created, location: @person }
       else
-        format.html { redirect_to groups_url, alert: "That person could not be added to that group" }
+        format.html { redirect_to groups_url, alert: "#{@membership.person.name} could not be added to #{@membership.group.name}" }
         format.json { render json: @membership.errors, status: :unprocessable_entity }
       end
     end
@@ -19,7 +19,7 @@ class MembershipsController < ApplicationController
     @membership.destroy
 
     respond_to do |format|
-      format.html { redirect_to person_url(@membership.person_id) }
+      format.html { redirect_to person_url(@membership.person_id), notice: "#{@membership.person.name} has been removed from #{@membership.group.name}" }
       format.json { head :no_content }
     end
   end
